e017151b3212711d04b15757b6be9c9e61c30d5c
joseph khella <josephkhella2010@gmail.com>
2026-05-30 20:51:02 +0200

    add backend test

---
+++ backend/apiRoutes/getBooks.js
@@ -0,0 +1,40 @@
+/* import express from "express";
+import books from "../utilities/BooksArray.js";
+const router = express.Router();
+const booksArr = books;
+
+router.get("/books", async (req, res) => {
+  try {
+    return res
+      .status(200)
+      .json({ message: "Successfully get Books", books: booksArr });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "error with get Books", error: err });
+  }
+});
+
+export default router;
+ */
+
+const express = require("express");
+const books = require("../utilities/BooksArray");
+
+const router = express.Router();
+
+router.get("/books", (req, res) => {
+  try {
+    return res.status(200).json({
+      message: "Successfully got books",
+      books,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: "Error getting books",
+      error: err.message,
+    });
+  }
+});
+
+module.exports = router;
